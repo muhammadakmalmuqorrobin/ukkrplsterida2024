43d35b18cb850d50c61ddf82fb72314929c8cf46
muhammadakmalmuqorrobin <150991763+muhammadakmalmuqorrobin@users.noreply.github.com>
2024-02-24 14:51:56 +0700

    Add files via upload

---
+++ websitegalerifoto/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-info">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">WEB GALERI FOTO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+            <div class="navbar-nav me-auto">
+
+            </div>
+                <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+                <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-3">
+        <h1>SELAMAT DATANG di WEBSITE GALERI FOTO</h1>
+    </div>
+
+    <footer class="d-flex justify-content-center border-top mt-3 bg-lingth fixed-bottom">
+        <p>&copy; UKK RPL 2024 | Muhammad akmal muqorrobin</p>
+    </footer>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
